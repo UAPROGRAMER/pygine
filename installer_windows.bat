e0265bf343c8557b8759fb33c45b486f1db6cc30
Stas Yatskiv <stasyatskiu2008@gmail.com>
2024-06-18 22:16:18 +0300

    modified:   installer_windows.bat

--- installer_windows.bat
+++ installer_windows.bat
@@ -2,4 +2,5 @@ set cdirrectory=%cd%
 set pyginecpfrom=%cdirrectory%\pygine\
 cd /D C:\
 for /f %%i in ('python -m site --user-site') do set destenation=%%i
+echo "copying pygine from %pyginecpfrom% to %destenation%"
 robocopy %pyginecpfrom% %destenation% /E
